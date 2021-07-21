44d9ff4f82f71eef5f40a45c70d4833c23088232
ChandimaHerath <chandimaherath@hotmail.com>
2021-07-21 20:04:48 +0530

    ServerScoket initiated

--- src/lk/Chandima/learn/chatapp/ServerApp.java
+++ src/lk/Chandima/learn/chatapp/ServerApp.java
@@ -2,12 +2,16 @@ package lk.Chandima.learn.chatapp;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerApp {
 
     public static void main(String[] args) throws IOException {
 
         ServerSocket serverSocket = new ServerSocket(7575);
+
+        Socket socket = serverSocket.accept();
+
 
 
     }
