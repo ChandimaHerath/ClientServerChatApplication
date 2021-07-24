b75821ea3e40058dbdcebbf8906daddcf9a70b05
ChandimaHerath <chandimaherath@hotmail.com>
2021-07-24 22:52:13 +0530

    Client app Finalized

--- src/lk/Chandima/learn/chatapp/ClientApp.java
+++ src/lk/Chandima/learn/chatapp/ClientApp.java
@@ -1,7 +1,49 @@
 package lk.Chandima.learn.chatapp;
 
-public class ClientApp {
-    public static void main(String[] args) {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class ClientApp {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try (Socket clientSocket = new Socket("localhost", 7575)) {
+            System.out.println("Enter Your Name:");
+            String name = scanner.nextLine();
+
+            try (OutputStream outputStream = clientSocket.getOutputStream();
+                 OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+                 BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter)) {
+
+
+                while (true) {
+
+                    System.out.println("Enter Your Message:");
+                    String message = scanner.nextLine();
+
+                    if (message.equalsIgnoreCase("exit")) {
+                        System.exit(0);
+
+                    }
+
+                    bufferedWriter.write(name + ":" + message);
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+
+                }
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+
+        }
     }
 }
