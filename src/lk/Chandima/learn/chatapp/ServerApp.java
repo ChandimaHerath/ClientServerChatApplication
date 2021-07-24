f5f4afdd44a7740138bbe8813d9ac5c8a49a53c3
ChandimaHerath <chandimaherath@hotmail.com>
2021-07-24 19:33:54 +0530

    added client module

--- src/lk/Chandima/learn/chatapp/ServerApp.java
+++ src/lk/Chandima/learn/chatapp/ServerApp.java
@@ -8,38 +8,44 @@ public class ServerApp {
 
     public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(7575);
-        System.out.println("Server Initiated...!");
+//        ServerSocket serverSocket = new ServerSocket(7575);
+//        System.out.println("Server Initiated...!");
+//
+//        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+//            if(!serverSocket.isClosed()){
+//                try {
+//                    serverSocket.close();
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }));
+//
+//        while (true) {
+//            //returns the local socket of the server
+//            System.out.println("Server is Listening for Incoming Connections....");
+//            try {
+//                Socket localSocket = serverSocket.accept();
+//            } catch (IOException e) {
+//
+//                if(serverSocket.isClosed()){
+//                    break;
+//                }
+//            }
+//
+//
+//        }
+//
+//        System.out.println("Server is Shutting Down..!");
+//
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            if(!serverSocket.isClosed()){
-                try {
-                    serverSocket.close();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }));
-
-        while (true) {
-            //returns the local socket of the server
-            System.out.println("Server is Listening for Incoming Connections....");
-            try {
-                Socket localSocket = serverSocket.accept();
-            } catch (IOException e) {
-
-                if(serverSocket.isClosed()){
-                    break;
-                }
-            }
 
 
-        }
 
-        System.out.println("Server is Shutting Down..!");
 
-    }
+
+     }
 
 
 }
